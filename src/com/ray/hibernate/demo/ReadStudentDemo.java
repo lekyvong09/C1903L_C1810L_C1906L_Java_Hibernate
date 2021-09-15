4f3c4ab01f67a47ac0c8f45d2f4089136438fc29
lekyvong09 <lekyvong09@gmail.com>
2021-09-15 21:01:57 +0700

    Insert then Read Student

---
+++ src/com/ray/hibernate/demo/ReadStudentDemo.java
@@ -0,0 +1,58 @@
+package com.ray.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ray.hibernate.demo.entity.Student;
+
+public class ReadStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory from configuration file
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create a student
+			System.out.println("Create a student");
+			Student tempStudent = new Student("Tommy1", "Le1", "tommy1@email.com");
+
+			
+			// start transaction
+			session.beginTransaction();
+			
+			// save object
+			System.out.println("Saving the student");
+			session.save(tempStudent);
+
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Saved student. Generated Id: " + tempStudent.getId());
+			
+			
+			// get a new session to retrieve the student data
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			Student myStudent = session.get(Student.class, tempStudent.getId());
+			
+			System.out.println("Just created student: " + myStudent);
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Complete");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
