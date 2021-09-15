c13d2b56862ecb75cb64af16d5b3b167541fd5d9
lekyvong09 <lekyvong09@gmail.com>
2021-09-15 21:13:28 +0700

    Query data with HQL

---
+++ src/com/ray/hibernate/demo/HQLQueryStudentDemo.java
@@ -0,0 +1,55 @@
+package com.ray.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ray.hibernate.demo.entity.Student;
+
+public class HQLQueryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory from configuration file
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction
+			session.beginTransaction();
+			
+			// retrieve all Student object to List
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			
+			displayStudents(theStudents);
+			
+			
+			theStudents = session.createQuery("from Student s where s.firstName='Ray'").getResultList();
+			
+			displayStudents(theStudents);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Complete");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+		
+	}
+
+}
