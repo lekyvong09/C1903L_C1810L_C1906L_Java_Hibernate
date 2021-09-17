62ace3d5d962d6631eeeef8d4bcfdce19f1cf56c
lekyvong09 <lekyvong09@gmail.com>
2021-09-17 21:16:39 +0700

    OneToOne Bidirectional - get Instructor form InstructorDetail

---
+++ src/com/ray/hibernate/demo/OneToOne/BidirectionalGetInstructorDetail.java
@@ -0,0 +1,45 @@
+package com.ray.hibernate.demo.OneToOne;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ray.hibernate.demo.entity.Instructor;
+import com.ray.hibernate.demo.entity.InstructorDetail;
+
+public class BidirectionalGetInstructorDetail {
+
+	public static void main(String[] args) {
+		// create session factory from configuration file
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// start transaction
+			session.beginTransaction();
+			
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, 2);
+			
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			
+			System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+			
+			// commit
+			session.getTransaction().commit();
+			
+			System.out.println("Complete");
+			
+			
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
